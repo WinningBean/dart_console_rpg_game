9c63ce8952bb6c921a69def89f279ddd409ce05f
SeungBeen Wee <WinningBean@gmail.com>
2025-07-01 14:32:19 +0900

    init: 프로젝트 세팅

---
+++ bin/dart_console_rpg_game.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console_rpg_game/dart_console_rpg_game.dart' as dart_console_rpg_game;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_console_rpg_game.calculate()}!');
+}
---
+++ test/dart_console_rpg_game_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_console_rpg_game/dart_console_rpg_game.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
