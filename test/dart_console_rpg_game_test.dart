6f81f3bea932dc8e0d4a51f6ebb8c0d82a3579e0
SeungBeen Wee <WinningBean@gmail.com>
2025-07-01 17:59:40 +0900

    test: 캐릭터 초기화 및 상태 업데이트 test 추가

--- test/dart_console_rpg_game_test.dart
+++ test/dart_console_rpg_game_test.dart
@@ -1,4 +1,32 @@
+import 'package:dart_console_rpg_game/monster.dart';
+import 'package:dart_console_rpg_game/player.dart';
 import 'package:test/test.dart';
 
 void main() {
+  // 생성 및 초기화 테스트
+  test('Character creation test', () {
+    var character = Player(name: 'Hero', maxHealth: 100, attackPower: 20, defensePower: 10);
+    
+    expect(character.name, 'Hero');
+    expect(character.maxHealth, 100);
+    expect(character.health, 100); // 초기 체력은 최대 체력과 같아야 함
+    expect(character.attackPower, 20);
+    expect(character.defensePower, 10);
+  });
+
+  // 상태 업데이트 테스트
+  test('Character attack test', () {
+    var player = Player(name: 'Hero', maxHealth: 100, attackPower: 20, defensePower: 10);
+    var monster = Monster(name: 'Goblin', maxHealth: 50, attackPower: 15);
+    
+    player.attackMonster(monster);
+    monster.attack(player);
+    
+    expect(monster.health, lessThan(50));
+    expect(player.health, lessThan(100));
+
+    var prevHealth = player.health;
+    player.defend();
+    expect(player.health, greaterThan(prevHealth));
+  });
 }
